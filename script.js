9a8f5f5910b50c185a886fa07a18e74e56ff474b
Gabriela Petrov <gabriela.n.petrov@gmail.com>
2023-12-26 09:34:13 +0100

    testimonial

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 document.addEventListener('DOMContentLoaded', function () {
     const testimonialsContainer = document.querySelector('.testimonials');
+    const testimonialContainer = document.querySelector('.testimonial-container');
     const testimonialBlocks = document.querySelectorAll('.testimonial-block');
-    const totalTestimonials = testimonialBlocks.length;
-    const testimonialsPerPage = 3;
+    const testimonialsPerPage = 1; // Display one testimonial at a time
 
     let currentIndex = 0;
 
     function updateTestimonials() {
         testimonialBlocks.forEach((block, index) => {
-            const position = (index - currentIndex + totalTestimonials) % totalTestimonials;
+            const position = (index - currentIndex + testimonialBlocks.length) % testimonialBlocks.length;
             block.style.display = position < testimonialsPerPage ? 'inline-block' : 'none';
         });
     }
 
-    function moveTestimonials() {
-        currentIndex = (currentIndex + 1) % totalTestimonials;
-        updateTestimonials();
-    
-        // Check if we have reached the last testimonial, and reset to the first one
-        if (currentIndex === totalTestimonials - testimonialsPerPage) {
-            setTimeout(() => {
-                currentIndex = 0;
-                updateTestimonials();
-            }, 500);
+    function moveTestimonials(direction) {
+        if (direction === 'next') {
+            currentIndex = (currentIndex + 1) % testimonialBlocks.length;
+        } else if (direction === 'prev') {
+            currentIndex = (currentIndex - 1 + testimonialBlocks.length) % testimonialBlocks.length;
         }
+
+        updateTestimonials();
     }
 
-    setInterval(moveTestimonials, 5000);
+    function autoMoveTestimonials() {
+        moveTestimonials('next');
+    }
+
+    // Set interval for automatic movement
+    const intervalId = setInterval(autoMoveTestimonials, 5000);
+
+    // Button event listeners
+    document.querySelector('.next-button').addEventListener('click', function () {
+        moveTestimonials('next');
+    });
+
+    document.querySelector('.prev-button').addEventListener('click', function () {
+        moveTestimonials('prev');
+    });
+
+    // Clear interval on mouseover and restart on mouseout
+    testimonialsContainer.addEventListener('mouseover', function () {
+        clearInterval(intervalId);
+    });
+
+    testimonialsContainer.addEventListener('mouseout', function () {
+        intervalId = setInterval(autoMoveTestimonials, 5000);
+    });
+
     updateTestimonials(); // Initial update
 });
