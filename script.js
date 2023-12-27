f4470cf0acdd7bba6ca2e763b8332c5a9cee3761
Gabriela Petrov <gabriela.n.petrov@gmail.com>
2023-12-27 09:30:40 +0100

    testemonials

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const testimonialsPerPage = 1; // Display one testimonial at a time
 
     let currentIndex = 0;
+    let intervalId;
 
     function updateTestimonials() {
         testimonialBlocks.forEach((block, index) => {
             const position = (index - currentIndex + testimonialBlocks.length) % testimonialBlocks.length;
-            block.style.display = position < testimonialsPerPage ? 'inline-block' : 'none';
+            block.style.display = position < testimonialsPerPage ? 'block' : 'none';
         });
     }
 
@@ -28,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // Set interval for automatic movement
-    const intervalId = setInterval(autoMoveTestimonials, 6000);
+    intervalId = setInterval(autoMoveTestimonials, 6000);
 
     // Button event listeners
     document.querySelector('.next-button').addEventListener('click', function () {
@@ -50,46 +51,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
     updateTestimonials(); // Initial update
 });
-document.addEventListener('DOMContentLoaded', function () {
-    const currentPage = window.location.pathname.split('/').pop();
-    const navLinks = document.querySelectorAll('.nav-link');
-
-    navLinks.forEach(link => {
-        const linkPage = link.href.split('/').pop();
-        if (linkPage === currentPage) {
-            link.classList.add('active');
-        }
-    });
-});
-// script.js
-function calculateInsurance() {
-    var coverageType = document.getElementById("coverageType").value;
-    var vehicleValue = parseFloat(document.getElementById("vehicleValue").value);
-    var duration = parseInt(document.getElementById("duration").value);
-
-    if (isNaN(vehicleValue) || isNaN(duration)) {
-        alert("Please enter valid numbers for Vehicle Value and Policy Duration.");
-        return;
-    }
-
-    var basePrice;
-
-    switch (coverageType) {
-        case "basic":
-            basePrice = 100;
-            break;
-        case "standard":
-            basePrice = 200;
-            break;
-        case "premium":
-            basePrice = 300;
-            break;
-        default:
-            alert("Invalid coverage type selected.");
-            return;
-    }
-
-    var estimatedPrice = basePrice * (vehicleValue / 1000) * duration;
-
-    document.getElementById("result").innerText = estimatedPrice.toFixed(2);
-}
